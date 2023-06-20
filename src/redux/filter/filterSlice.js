10ce0a9c03bba062ef0488f7d3d2a73ffc7c0812
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-20 23:19:15 +0300

    Create filterSlice.js

---
+++ src/redux/filter/filterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { logOut } from 'redux/auth/operations';
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, { payload }) {
+      return payload;
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(logOut.fulfilled, state => (state = ''));
+  },
+});
+
+export const { setFilter } = filterSlice.actions;
+export const filterReducer = filterSlice.reducer;
