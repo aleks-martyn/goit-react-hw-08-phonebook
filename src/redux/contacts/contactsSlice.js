fc54b7021eb5e0b882869e413a83f241d61f8b57
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-20 22:55:05 +0300

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -2,6 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from './operations';
 import { logOut } from 'redux/auth/operations';
 
+const initialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+
 const handlePending = state => {
   state.isLoading = true;
 };
@@ -22,3 +28,35 @@ const handleFulfilledAddContact = (state, { payload }) => {
   state.error = null;
   state.items = [payload, ...state.items];
 };
+
+const handleFulfilledDeleteContact = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items = state.items.filter(({ id }) => id !== payload.id);
+};
+
+const handleFulfilledLogOut = state => {
+  state.items = [];
+  state.error = null;
+  state.isLoading = false;
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.fulfilled, handleFulfilledContacts)
+      .addCase(addContact.fulfilled, handleFulfilledAddContact)
+      .addCase(deleteContact.fulfilled, handleFulfilledDeleteContact)
+      .addCase(logOut.fulfilled, handleFulfilledLogOut)
+      .addMatcher(action => {
+        action.type.endsWith('/pending');
+      }, handlePending)
+      .addMatcher(action => {
+        action.type.endsWith('/rejected');
+      }, handleRejected);
+  },
+});
+
+export const contactsReducer = contactsSlice.reducer;
