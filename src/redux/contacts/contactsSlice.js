a59039a5599a046a84653ed6f2069e8ac272f47f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-20 19:12:59 +0300

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,1 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
+import { logOut } from 'redux/auth/operations';
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = payload;
+};
+
+const handleFulfilledContacts = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items = payload;
+};
+
+const handleFulfilledAddContact = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items = [payload, ...state.items];
+};
