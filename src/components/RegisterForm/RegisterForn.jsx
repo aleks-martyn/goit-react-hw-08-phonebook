b03761e897d3543dfcd14081516a3622f821f8ef
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-21 16:41:14 +0300

    Update RegisterForn.jsx

--- src/components/RegisterForm/RegisterForn.jsx
+++ src/components/RegisterForm/RegisterForn.jsx
@@ -17,4 +17,22 @@ export const RegisterForm = () => {
     );
     form.reset();
   };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <label>
+        Username
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
 };
