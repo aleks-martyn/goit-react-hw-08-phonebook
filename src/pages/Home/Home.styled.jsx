f66eefdf1586bc9d5eff5d553bf38af3844f09f7
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-21 22:50:44 +0300

    Update Home.styled.jsx

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,2 +1,10 @@
 import styled from '@emotion/styled';
 
+export const Container = styled.div`
+  width: 1200px;
+  minHeight: 'calc(100vh - 50px)',
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+`;
