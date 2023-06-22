1597ed3cf0caeb40a5547c31120caf04ab505089
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-23 00:30:22 +0300

    Update AuthNav.styled.jsx

--- src/components/AuthNav/AuthNav.styled.jsx
+++ src/components/AuthNav/AuthNav.styled.jsx
@@ -2,6 +2,10 @@ import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
 export const Link = styled(NavLink)`
+  display: inline-block;
+  text-decoration: none;
+  padding: 12px;
+
   :nth-child(2n + 1) {
     margin-right: 20px;
   }
