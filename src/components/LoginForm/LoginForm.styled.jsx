353f77ba818489aa528179c89bcc9f2db3af4e34
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-23 00:29:51 +0300

    Create LoginForm.styled.jsx

---
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
