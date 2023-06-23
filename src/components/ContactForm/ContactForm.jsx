e8c924a2178b8425ee9a799c3ed9ed671cf92863
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-23 15:18:29 +0300

    Update ContactForm.jsx

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,65 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
+import { addContact } from 'redux/contacts/operations';
+import { selectContacts } from 'redux/contacts/selectors';
+import { Form, FormLabel, Input } from './ContactForm.styled';
+
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    const contact = {
+      name: form.elements.name.value,
+      number: form.elements.number.value,
+    };
+
+    if (
+      contacts.find(
+        ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+      )
+    ) {
+      alert(`${contact.name} is already in contacts`);
+      return;
+    }
+
+    dispatch(addContact(contact));
+    form.reset();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormLabel htmlFor={nameInputId}>
+        <span>Name</span>
+      </FormLabel>
+      <Input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        id={nameInputId}
+      />
+
+      <FormLabel htmlFor={numberInputId}>
+        <span>Number</span>
+      </FormLabel>
+      <Input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        id={numberInputId}
+      />
+
+      <button type="submit">Add contact</button>
+    </Form>
+  );
+};
