49894982905ab53e8f78e140b9c4ea7ae33640ed
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-23 15:37:47 +0300

    Update selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,5 +1,17 @@
 import { createSelector } from '@reduxjs/toolkit';
+import { selectFilter } from 'redux/filter/selectors';
 
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
