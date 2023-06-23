9aa866e5cf57e58c2b7bab6c6647c15ced6f8f79
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-23 20:43:01 +0300

    Update Contacts.jsx

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,13 +1,14 @@
 import { ContactForm } from 'components/ContactForm';
 import { ContactList } from 'components/ContactList';
 import { Filter } from 'components/Filter';
+import { Container } from './Contacts.styled';
 
 export default function Contacts() {
   return (
-    <div>
+    <Container>
       <ContactForm />
       <Filter />
       <ContactList />
-    </div>
+    </Container>
   );
 }
