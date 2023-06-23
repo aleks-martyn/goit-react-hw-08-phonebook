d3a1218fd50e1c026657e74b43063db71074e5f7
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-23 22:11:57 +0300

    Update AppBar.styled.jsx

--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -1,8 +1,13 @@
 import styled from '@emotion/styled';
 
-export const Header = styled.header`
+export const Container = styled.div`
   display: flex;
   justify-content: space-between;
+  width: 1000px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: auto;
+  margin-right: auto;
   align-items: center;
   margin-bottom: 16px;
   border-bottom: 1px solid #2a363b;
