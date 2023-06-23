f7fb90c566471cbc67a3ff3e95e418eeb7053713
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-23 20:06:58 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { selectFilter } from 'redux/filter/selectors';
 import { setFilter } from 'redux/filter/filterSlice';
+import { FilterLabel, Text } from './Filter.styled';
 
+export const Filter = () => {
+  const filter = useSelector(selectFilter);
+  const dispatch = useDispatch();
+
+  const handleChange = event =>
+    dispatch(setFilter(event.currentTarget.value.trim()));
+
+  return (
+    <FilterLabel>
+      <Text>Find contacts by name</Text>
+      <input type="text" value={filter} onChange={handleChange} />
+    </FilterLabel>
+  );
+};
