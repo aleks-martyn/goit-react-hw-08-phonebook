bfa64abee32eb240fe54342a410a422f2ede2923
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-23 22:12:11 +0300

    Update Contacts.styled.jsx

--- src/pages/Contacts/Contacts.styled.jsx
+++ src/pages/Contacts/Contacts.styled.jsx
@@ -3,4 +3,9 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
 `;
