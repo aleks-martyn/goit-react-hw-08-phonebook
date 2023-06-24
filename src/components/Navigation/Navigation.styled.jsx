da047d0dd470ad32f645ea7c6735d9cdf2fa6883
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-24 16:05:42 +0300

    Update Navigation.styled.jsx

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -5,4 +5,10 @@ export const Link = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
   padding: 12px;
+  color: #212121;
+  font-size: 20px;
+
+  &.active {
+    color: teal;
+  }
 `;
