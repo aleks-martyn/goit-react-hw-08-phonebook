48fbda35607c391bd747c9a746d990504d20f438
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-24 22:31:11 +0300

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,10 @@
 import { LoginForm } from 'components/LoginForm';
-import { Container } from './Login.styled';
+import { Container, Title } from './Login.styled';
 
 export default function Login() {
   return (
     <Container>
-      <h2>Log in</h2>
+      <Title>Log in</Title>
       <LoginForm />
     </Container>
   );
