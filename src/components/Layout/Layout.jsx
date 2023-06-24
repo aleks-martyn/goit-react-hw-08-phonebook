f38decb9717b627fc15072a267b0e18fd95720ca
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-24 22:24:31 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AppBar } from 'components/AppBar';
+import { Spinner } from 'components/Loader';
 
 export const Layout = () => {
   return (
     <div>
       <AppBar />
-      <Suspense fallback={null}>
+      <Suspense fallback={<Spinner />}>
         <Outlet />
       </Suspense>
     </div>
