887765077695c3a113295c55c85707e3e4709f14
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-24 19:44:31 +0300

    Update ContactListItem.styled.jsx

--- src/components/ContactListItem/ContactListItem.styled.jsx
+++ src/components/ContactListItem/ContactListItem.styled.jsx
@@ -1,19 +1,33 @@
 import styled from '@emotion/styled';
+import { Person2Outlined } from '@mui/icons-material';
 
 export const Item = styled.li`
   display: flex;
   gap: 15px;
   align-items: center;
+  justify-content: space-between;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 5px;
+
+  :nth-of-type(2n + 1) {
+    background-color: #dee2e3;
+  }
 `;
 
 export const Text = styled.p`
   font-size: 17px;
   font-weight: 500;
+  color: #212121;
 `;
 
-export const Marker = styled.span`
-  width: 5px;
-  height: 5px;
-  background-color: #000;
-  border-radius: 50%;
+export const PersonIcon = styled(Person2Outlined)`
+  color: #212121;
+`;
+
+export const Wrap = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
 `;
