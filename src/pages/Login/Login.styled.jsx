eb520de27fb791a891a0ab3ff00969a2f418bfab
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-24 16:05:55 +0300

    Update Login.styled.jsx

--- src/pages/Login/Login.styled.jsx
+++ src/pages/Login/Login.styled.jsx
@@ -1,5 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+  width: 1000px;
+  min-height: calc(100vh - 50px);
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 100px;
+  padding-left: 15px;
+  padding-right: 15px;
   text-align: center;
 `;
